1608ddc4b83676b0c27fd4aba8dc19d7ae763b02
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2022-02-06 20:41:49 +0100

    updated file with axios (get, post), added new states and NewCocktail component

--- sip-sip-client/src/components/CocktailList.js
+++ sip-sip-client/src/components/CocktailList.js
@@ -1,27 +1,78 @@
-import { useGlobalContext } from "../context";
-import Loading from './Loading';
-import Cocktail from './Cocktail';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Cocktail from '../components/Cocktail';
+import NewCocktail from '../components/NewCocktail';
+
 
 const CocktailList = () => {
-  const { cocktails, loading } = useGlobalContext();
-  if (loading) {
-    return <Loading />
-  }
-  if (cocktails.length < 1) {
+    const [allDrinks, setAllDrinks] = useState([]);
+    const [refetchFlag, setRefetchFlag] = useState(false);
+    const [newCocktailData, setNewCocktailData] = useState({
+        name: "",
+        category: "",
+        type: "",
+        glass: "",
+        instructions: "",
+        url: "",
+        ingredient1: "",
+        ingredient2: "",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        ingredient6: "",
+        ingredient7: "",
+      });
+    
+      useEffect(() => {
+          axios
+              .get("http://localhost:8000/drinks")
+              .then(response => setAllDrinks(response.data))
+              .catch(error => console.log(error))
+      }, [refetchFlag])
+
+      const base_url = "http://localhost:8000/drinks";
+
+      const addNewCocktail = async (e) => {
+        e.preventDefault();
+        await axios.post(base_url, {
+          name: newCocktailData.name,
+          category: newCocktailData.category,
+          type: newCocktailData.type,
+          glass: newCocktailData.glass,
+          instructions: newCocktailData.instructions,
+          url: newCocktailData.url,
+          ingredient1: newCocktailData.ingredient1,
+          ingredient2: newCocktailData.ingredient2,
+          ingredient3: newCocktailData.ingredient3,
+          ingredient4: newCocktailData.ingredient4,
+          ingredient5: newCocktailData.ingredient5,
+          ingredient6: newCocktailData.ingredient6,
+          ingredient7: newCocktailData.ingredient7,
+        })
+        setNewCocktailData({});
+        setRefetchFlag(!refetchFlag);
+      }
+    
     return (
-      <h2>No cocktails found.</h2>
-    )
-  }
-  return (
-    <section className="section">
-      <h2 className="section-title">Cocktails</h2>
-      <div className="cocktails-container">
-        {cocktails.map((item) => {
-          return <Cocktail key={item.id} {...item} />
-        })}
-      </div>
-    </section>
-  );
+        <section className= "section" >
+            <h2 className="section-title">My Cocktails</h2>
+            <div className = "cocktails-container">
+                {
+                allDrinks.map((item) => {
+                    return <Cocktail key={ item.id } {...item} />
+                })}
+            </div>
+            <div>
+                <h2 className="form-title">ADD NEW COCKTAIL</h2>
+                <NewCocktail
+                    newCocktailData={newCocktailData}
+                    setNewCocktailData={setNewCocktailData}
+                    addNewCocktail={addNewCocktail}
+                />
+            </div>
+        </section>
+        
+  )
 };
 
 export default CocktailList;
