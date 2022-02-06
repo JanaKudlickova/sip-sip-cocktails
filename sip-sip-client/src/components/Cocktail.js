52a2fa2ee394aa04f0db73b34f56f7cef3d5b663
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2022-02-06 20:38:23 +0100

    updated file due to new db

--- sip-sip-client/src/components/Cocktail.js
+++ sip-sip-client/src/components/Cocktail.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
-const Cocktail = ({id, name, image, info, category}) => {
+const Cocktail = ({id, name, url, type, category}) => {    
   return(
       <article className="cocktail">
           <div>
-              <img src={image} alt={name} />
+              <img src={url} alt={name} />
           </div>
           <div className="cocktail-info">
               <h3>{name}</h3>
               <h4>{category}</h4>
-              <p>{info}</p>
+              <p>{type}</p>
               <Link to={`/cocktail/${id}`} className="btn-details"> SEE DETAILS</Link>
           </div>
       </article>
@@ -16,3 +16,4 @@ const Cocktail = ({id, name, image, info, category}) => {
 };
 
 export default Cocktail;
+
