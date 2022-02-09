29c89eb2f1e4c8bd626c02423137a71d8c1475e6
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2022-02-09 10:11:13 +0100

    Change the connection to pool connection Co-authored-by: Carlos Orellana <carlosaore@users.noreply.github.com>

--- server/db.js
+++ server/db.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT, 
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  multipleStatements: true,
-  decimalNumbers: true
+const connection = mysql.createPool({
+	connectionLimit: 10,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
 });
 
+
 module.exports = connection;
+
